36b2b07eabc0e0be556fd42c5c64721283f428a4
avidbiscuit <sean.magennis001@outlook.com>
2026-08-02 21:27:56 +0100

    Added a Minecart Chest check for using the Metal Detector item

--- src/main/java/net/avidbiscuit/tutorialmod/block/custom/SoundBlock.java
+++ src/main/java/net/avidbiscuit/tutorialmod/block/custom/SoundBlock.java
@@ -4,14 +4,22 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.RedstoneWireBlock;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
 
@@ -57,5 +65,11 @@ public class SoundBlock extends Block {
                 1f,
                 1f
         );
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.sound_block.tooltip").formatted(Formatting.GRAY));
+        super.appendTooltip(stack, world, tooltip, options);
     }
 }
--- src/main/java/net/avidbiscuit/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/avidbiscuit/tutorialmod/item/custom/MetalDetectorItem.java
@@ -3,23 +3,35 @@ package net.avidbiscuit.tutorialmod.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.ChestMinecartEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
         super(settings);
     }
 
+
+
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         if(!context.getWorld().isClient()) {
             BlockPos positionClicked = context.getBlockPos();
             PlayerEntity player = context.getPlayer();
+
+            boolean foundMinecart = findMinecart(context.getWorld(), positionClicked, player);
             boolean foundBlock = false;
 
             for(int i = 0; i <= positionClicked.getY() + 64; i++){
@@ -33,14 +45,18 @@ public class MetalDetectorItem extends Item {
                 }
             }
 
-            if(!foundBlock){
+
+
+            if(!foundBlock && !foundMinecart){
                 player.sendMessage(Text.literal("No Valuables Found!"));
             }
-
         }
+
+
 
         context.getStack().damage(1, context.getPlayer(),
                 playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+
 
         return ActionResult.SUCCESS;
     }
@@ -50,8 +66,46 @@ public class MetalDetectorItem extends Item {
                 "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), false);
     }
 
+    //New method to find a minecart with a chest
+    private boolean findMinecart(World world, BlockPos pos, PlayerEntity player) {
+        List<ChestMinecartEntity> minecarts = world.getEntitiesByClass(
+                ChestMinecartEntity.class,
+                new net.minecraft.util.math.Box(pos).expand(5),
+                entity -> true
+
+        );
+
+        if (!minecarts.isEmpty()) {
+            ChestMinecartEntity cart = minecarts.get(0);
+
+            player.sendMessage(Text.literal(
+                    "Found Minecart Chest at (" +
+                            cart.getBlockX() + ", " +
+                            cart.getBlockY() + ", " +
+                            cart.getBlockZ() + ")"
+            ), false);
+
+                return true;
+        }
+            return false;
+    }
+
     private boolean isValuableBlock(BlockState state) {
         return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+
+
     }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.metal_detector.tooltip").formatted(Formatting.GRAY));
+        super.appendTooltip(stack, world, tooltip, context);
+
+
+    }
+
+
+
 }
+
 
